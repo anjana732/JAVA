9e38af647e585015bc5575514e2030c21c117f1d
anjana732 <anjanasingh1257@gmail.com>
2023-12-24 12:41:20 +0530

    Access modifiers

---
+++ anjana.java
@@ -0,0 +1,18 @@
+class A{
+	public static void main(String[] agrs){
+		System.out.println("Class A");
+	}
+}
+class B{
+	public static void main(String[] agrs){
+		System.out.println("Class B");
+	}
+}
+class C{
+	public static void main(String[] agrs){
+		System.out.println("Class C");
+	}
+}
+class D{
+	
+}
